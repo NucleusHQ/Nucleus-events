c2ef685af31eb0170351e9ef033ef71486a27cee
NucleusHQ <hi@nucleushq.io>
2023-12-03 17:46:24 +0530

    Data added for Async JS

--- src/data/events.js
+++ src/data/events.js
@@ -70,6 +70,75 @@ export const events = {
         targetAudience: ["Early career developer", "Self-taught programmers", "Aspiring developers", "College students"],
         isRunning: true
     },
+    102: {
+        category: "tofu",
+        eventId: 102,
+        type: "free",
+        title: "Learn Asynchronous JavaScript The Easy Way",
+        checkoutUrl: "",
+        description: "",
+        guestInfo: {
+            fullName: "Harish Neel",
+            designation: "Software Developer, Samsung, USA | Ex-Freshworks | Ex-Zoho",
+            imgUrl: "",
+            bio: `Harish's journey as part of the core team of 10 developers at samsung.com/us, contributing to its remarkable $1875 million ARR, has fueled his passion for collaborating with developers and entrepreneurs globally. With a penchant for problem-solving through coding, he has crafted over 20 software products for international clients, some of which now boast 8K+ Monthly Active Users in the US.<br><br>
+
+            He firmly believes in the power of consistent, mindful effort as the cornerstone of success, encouraging fellow developers to embrace the process rather than fixate on the outcome.`
+        },
+        dateInfo: {
+            month: "October",
+            monthShort: "Oct",
+            date: 28,
+            day: "Saturday",
+            startTime: "11:00 AM",
+            endTime: "9:00 PM",
+        },
+        about: ["In this masterclass, Harish Neel, Software Engineer at Samsung, will help you dive deep into the world of asynchronous programming with JavaScript. Asynchronous JavaScript is a crucial concept for backend engineers and developers working with web applications, as it enables efficient handling of tasks that may take time to complete.", 
+        "The Masterclass will begin with an exploration of the fundamentals of asynchronous programming, providing a solid understanding of its importance and how it works in JavaScript. You will then learn how to work with callbacks, promises, and the modern async/await syntax, which are powerful tools for managing asynchronous tasks and improving code readability. It will also cover best practices for error handling and demonstrate how to effectively manage the control flow of asynchronous operations."
+    ],
+        whyInfo: {
+            title: "Why Understand Asynchronous JavaScript",
+            content: [
+                "<strong>⚡ Boost Performance:</strong> Learn how asynchronous JavaScript speeds up applications by handling tasks without blocking other processes.",
+                "<strong>🌐 Web Dev Standard:</strong> Uncover why asynchronous programming is a fundamental skill in modern web development, enhancing user experience.",
+                "<strong>🤖 Real-world Apps:</strong> Explore practical examples of asynchronous JavaScript in action, from managing API requests to creating dynamic interfaces.",
+                "<strong>🛠️ Master Modern Dev:</strong> Stay ahead in web development by mastering asynchronous patterns crucial to contemporary practices.",
+                "<strong>💡 Problem-Solving:</strong> Develop the skill of simultaneous task handling, solving challenges in complex projects.",
+                "Ready to advance your skills? Click above to <strong>register</strong> for the insights masterclass and level up in asynchronous JavaScript. Join us for a knowledge-packed journey!"
+            ]
+        },        
+        faqList: [{
+            "question": "I have registered but I have not received any confirmation email",
+            "answer": "If so, kindly contact support@nucleushq.io and our awesome team will help you out right awa"
+        },
+        {
+            "question": "Is the Program Live or Pre-recorded?",
+            "answer": "All the masterclasses we conduct are completely LIVE so that you can get your questionuestions clarified at any point during the session. :)"
+        }, {
+            "question": "I have a full-time job. Not sure if I can make it. Will you be sharing the recordings?",
+            "answer": "Absolutely! The live-class recordings will be provided to you but we recommend you attend the live-classes as that gives you an opportunity to clear your doubts."
+        }, {
+            "question": "Is this a program for advanced experienced developers?",
+            "answer": "Absolutely not. This is for absolute beginners who would like to get into coding but also for developers who are not confident with their Web Dev skills."
+        }, {
+            "question": "Will there be a certificate on completion?",
+            "answer": "Yes! You will receive a certificate on completion."
+        }, {
+            "question": "Why is the masterclass FREE despite it being value-packed?",
+            "answer": "We built Nucleus with the sole purpose of making education accessible to millions of people at scale"
+        }, {
+            "question": "Can I record the session for personal use later?",
+            "answer": `The content is owned by Nucleus. It is not allowed to record and use it for personal use. Doing so will lead to legal repercussions. But here’s what you should do 😛 
+            <br><br> 
+            Our best learners usually take notes or create mind maps and share them with the community. So, if you want to revisit your learnings or see what was in the content of the session, just take notes and revisit them in the future.
+            <br><br>
+            Not only does it increase learning output also makes the overall learning experience much much better. Don’t believe it? Just try it :)
+            `
+        }],
+        takeaways: ["A solid understanding of asynchronous programming and its importance in JavaScript.", "Proficiency in working with callbacks, promises, and async/await syntax", "Ability to effectively manage the control flow of asynchronous tasks", "Easily answering difficult interview questions"],
+        targetAudience: ["Early career developer", "Self-taught programmers", "Aspiring developers", "College students"],
+        isRunning: true
+    },
     203: {
         category: "tofu",
         eventId: 203,
